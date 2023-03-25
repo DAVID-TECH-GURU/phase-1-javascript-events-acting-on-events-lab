dc5342208036155e4f1e8469d1318bfae569f64d
DAVID-TECH-GURU <davidmaina823123@gmail.com>
2023-03-25 03:35:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-// Your code here
+//moveDodgerLeft()
+
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    document.addEventListener('keydown', function(event) {
+      console.log(event.key);
+    });
+  }
+
+  function moveDodgerLeft() {
+    document.addEventListener('keydown', function(event) {
+      if (event.key === "ArrowLeft") {
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+  
+        dodger.style.left = `${left - 1}px`;
+      }
+    });
+  }
+
+  moveDodgerLeft();
+
+
+
+  //moveDodgerRight()
+
+  function moveDodgerRight() {
+    document.addEventListener('keydown', function(event) {
+      if (event.keyCode === 39) {
+        let leftNumbers = dodger.style.left.replace('px', '');
+        let left = parseInt(leftNumbers, 10);
+  
+        if (left < 360) {
+          dodger.style.left = `${left + 5}px`;
+        }
+      }
+    });
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+
+
+  //final code
+
+  constdodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+//my test trial code for the lab
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  
